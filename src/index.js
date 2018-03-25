a5a4a2f98de37b3f44196eb1f3a5d255a916620b
Salvador Guzman <guzmansalv@gmail.com>
2018-03-24 17:30:38 -0700

    finalized show scraper

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const rxjs = require('rxjs');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const s = require('string');
+const _ = require('lodash');
 
 function get(url) {
   return rxjs.Observable.fromPromise(rp({
@@ -12,6 +13,7 @@ function get(url) {
 
 rxjs.Observable.of(
     'http://www3.gogoanime.tv/category/a-channel',
+    'https://gogoanime.se/category/one-punch-man'
     )
     .flatMap(get)
     .map(cheerio.load)
@@ -22,32 +24,33 @@ rxjs.Observable.of(
           })
         .map(t => `http://www3.gogoanime.tv/load-list-episode?ep_start=${t.start}&ep_end=${t.end}&id=${t.id}&default_ep=0`)
         .flatMap(get)
+        .retry()
         .map(cheerio.load)
         .flatMap(t => t('#episode_related > li > a'))
-        .map(t => ({
-          href: t.attribs.href,
-          ep: parseInt(s(cheerio.load(t)('div.name').text()).chompLeft(' EP ').s) - 1
-        }))
+        .pluck('attribs', 'href')
+        .map(a => s(a).trim().s)
         .reduce((a,b) => Array.prototype.concat(a, [b]), [])
-        .map(u => ({
-          title: t('div.anime_info_body_bg > h1').text(),
-          eps: u.reduce((a, b) =>  {
-            a[b.ep] = s(b.href).trim().s;
-            return a;
-          }, [])
+        .map(a => _.reverse(a))
+        .flatMap(a => rxjs.Observable.forkJoin(
+            a
+                .map(b => `http://www3.gogoanime.tv${b}`)
+                .map(b =>
+                    rxjs.Observable.of(b)
+                        .flatMap(get)
+                        .retry()
+                        .map(cheerio.load)
+                        .map(c => c('div.download-anime > a[href]'))
+                        .pluck('0', 'attribs', 'href')
+                        .flatMap(get)
+                        .map(cheerio.load)
+                        .map(c => c('div.dowload > a[href]'))
+                        .pluck('0', 'attribs', 'href')
+                )
+        ))
+        .map(a => ({
+          title: t('.anime_info_body_bg > h1').text(),
+          eps: a
         }))
-        .flatMap(v => rxjs.Observable.from(v.eps)
-            .map(a => `http://www3.gogoanime.tv${a}`)
-            .flatMap(get)
-            .retry()
-            .map(cheerio.load)
-            .flatMap(a => a('div.download-anime > a'))
-            .pluck('attribs', 'href')
-            .flatMap(get)
-            .map(cheerio.load)
-            .map(a => a('div.dowload > a'))
-            .pluck('0', 'attribs', 'href')
-        )
     )
     .subscribe(
         console.log,
